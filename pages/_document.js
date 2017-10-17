41e7bff9237a88cf4e2accd185fd1c1e7330faa8
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-10-17 14:04:50 +0330

    feat: add heap analytics

--- pages/_document.js
+++ pages/_document.js
@@ -34,6 +34,11 @@ class MyDocument extends Document {
 
           <style dangerouslySetInnerHTML={{ __html: globalStyles }} />
           <link href="https://fonts.googleapis.com/css?family=Miriam+Libre:700|Nunito:400,700" rel="stylesheet" />
+
+          <script type="text/javascript">
+            window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+              heap.load("2947346854");
+          </script>
         </Head>
         <body>
           <Main />
