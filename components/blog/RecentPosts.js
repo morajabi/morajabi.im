b4be6c3225f13f491879ec83fb64941e07418bf3
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-07-21 21:43:05 +0430

    Add styles to the recent posts section

--- components/blog/RecentPosts.js
+++ components/blog/RecentPosts.js
@@ -25,10 +25,50 @@ const RecentPosts = ({ posts }) => (
 export default RecentPosts
 
 // Styles
-const Wrapper = styled.section``
+const Wrapper = styled.section`
+  margin-top: 60px;
+`
 
-const Title = styled.h2``
+const Title = styled.h2`
+  margin-bottom: 20px;
+  font-family: 'IBM Plex Serif', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue';
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 1;
+  color: #bbb;
+`
 
-const List = styled.ul``
+const List = styled.ul`
+  padding: 0;
+`
 
-const Post = styled.li``
+const Post = styled.li`
+  position: relative;
+  margin-bottom: 20px;
+  list-style: none;
+
+  font-size: 22px;
+  font-weight: bold;
+  line-height: 1.15;
+  color: #21506a;
+
+  &:hover {
+    color: #000;
+  }
+
+  a {
+    color: currentColor;
+    text-decoration: none;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: calc(50% - 4px);
+    left: -22px;
+    width: 8px;
+    height: 8px;
+    background: #e0e0e0;
+  }
+`
